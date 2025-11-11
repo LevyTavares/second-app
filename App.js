b35b23a6350fe1911c92dec3a2ff1ac5c02382b0
Seu Nome <seu-email@exemplo.com>
2025-11-11 19:50:00 -0300

    Remove o componente View do App e mantém apenas o texto de boas-vindas

--- App.js
+++ App.js
@@ -4,9 +4,9 @@ import React, { Component } from "react";
 class App extends Component {
   render() {
     return (
-      <View>
+
         <Text>Bem vindo ao app de teste!</Text>
-      </View>
+
     );
   }
 }
