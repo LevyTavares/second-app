4f1c8382f5b4faa12795eb032ef6983f5419cf51
Seu Nome <seu-email@exemplo.com>
2025-11-11 20:35:52 -0300

    feat: adiciona suporte a SASS no projeto e atualiza dependências no package.json e package-lock.json

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,21 @@
 const { getDefaultConfig } = require("expo/metro-config");
+const path = require("path");
 
 const config = getDefaultConfig(__dirname);
-// Adiciona suporte a scss/sass
-config.resolver.sourceExts.push("scss", "sass");
-config.transformer.babelTransformerPath = require.resolve(
-  "react-native-sass-transformer"
-);
+
+// Garante que não duplica extensões
+const sourceExts = config.resolver.sourceExts || [];
+["scss", "sass"].forEach((ext) => {
+  if (!sourceExts.includes(ext)) sourceExts.push(ext);
+});
+config.resolver.sourceExts = sourceExts;
+
+// Define transformer do SASS
+config.transformer = {
+  ...config.transformer,
+  babelTransformerPath: require.resolve("react-native-sass-transformer"),
+};
+
+module.exports = config;
 
 module.exports = config;
